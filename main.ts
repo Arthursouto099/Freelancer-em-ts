cb22d1484892433f1e97da3e3a457a56b3073ee5
Arthur Souto <arthursantostavarescontatos@gmail.com>
2024-07-09 13:32:29 -0300

    ainda sem solução

--- main.ts
+++ main.ts
@@ -5,4 +5,17 @@ import {Avaliacao} from "./Avaliacao"
 
 
 
+const p1 = new Projeto('CALCULADORA', 'calculadoraaa pika', null)
+
+
+const f1 = new Freelancer('BRUNO', 'BRUNOR@GMAIL', 'FRONTEND')
+
+p1.atribuirFreelancer(f1)
+
+
+const p3 = new Projeto('CALCULADORA', 'calculadoraaa pika', f1)
+
+f1.adicionarProjeto(p3)
+
+const sla = new Avaliacao(f1, p1, 10)
 
